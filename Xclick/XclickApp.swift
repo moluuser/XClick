34587722469feeeb63fa3fbb6df536034a348d70
Chenyang <moluuser@gmail.com>
2023-07-27 09:24:26 +0800

    Initial Commit

---
+++ Xclick/XclickApp.swift
@@ -0,0 +1,17 @@
+//
+//  XclickApp.swift
+//  Xclick
+//
+//  Created by Chenyang on 2023/7/27.
+//
+
+import SwiftUI
+
+@main
+struct XclickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
